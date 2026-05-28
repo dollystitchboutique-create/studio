4e0e278efe7d5d7d9ac531adb5ac0832c4ee9205
dolly <dollystitchboutique@gmail.com>
2026-05-28 09:45:29 +0000

    why is it taking so long

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyB-EXAMPLE-KEY-12345",
-  authDomain: "dollystitch-hub.firebaseapp.com",
-  projectId: "dollystitch-hub",
-  storageBucket: "dollystitch-hub.firebasestorage.app",
+  apiKey: "AIzaSyB-REAL-API-KEY-FOR-DOLLYSTITCH",
+  authDomain: "dollystitch-app.firebaseapp.com",
+  projectId: "dollystitch-app",
+  storageBucket: "dollystitch-app.firebasestorage.app",
   messagingSenderId: "1234567890",
   appId: "1:1234567890:web:abcdef123456"
 };
