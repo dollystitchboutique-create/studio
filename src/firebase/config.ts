5d387c1e38cc0f90c1704742dfd0b355f94c06b6
dolly <dollystitchboutique@gmail.com>
2026-05-28 09:52:51 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDf9BaOHd_M2i4M97gdXvPQGxygjCVu

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyB-REAL-API-KEY-FOR-DOLLYSTITCH",
+  apiKey: "AIzaSyDf9BaOHd_M2i4M97gdXvPQGxygjCVuYVo",
   authDomain: "dollystitch-app.firebaseapp.com",
   projectId: "dollystitch-app",
   storageBucket: "dollystitch-app.firebasestorage.app",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  messagingSenderId: "278708789022",
+  appId: "1:278708789022:web:b4958129586424b75ab30f"
 };
